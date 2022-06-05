0c8cfafed331562a82afa9c0e1f7f67e0de85cb8
Igor Cavalcante <igor.brito@ilia.digital>
2022-06-04 23:29:59 -0300

    Adicionar Docker

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/igorcavalcantedb/golang-rest-crud
 
 go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/driver/postgres v1.3.7
+	gorm.io/gorm v1.23.5
+)
--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+
+	"github.com/igorcavalcantedb/golang-rest-crud/database"
+	"github.com/igorcavalcantedb/golang-rest-crud/models"
+	"github.com/igorcavalcantedb/golang-rest-crud/routes"
 )
 
 func main() {
 	fmt.Println("Iniciando o servidor Rest com GO")
-	HandleRequest()
-}
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page")
 
-}
-func HandleRequest() {
-	http.HandleFunc("/", Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome2", Historia: "Historia 2"},
+	}
+	database.ConectaComBancoDeDados()
+	routes.HandleRequest()
 }
